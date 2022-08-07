a3f0359eb0ba8f65deac6a65b9ae5760471903ae
sitorasu <13363542+sitorasu@users.noreply.github.com>
2022-08-07 22:26:58 +0900

    テスト用の関数と個々のテストを分離

---
+++ test-format-checker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source test.sh
+source format-checker-functions.sh
+
+check_stdout 9 count_chars_in_line 'abcdefghi'
+check_stdout 9 count_chars_in_line '  ab  c  '
+check_stdout 9 count_chars_in_line 'あいうabc'
+check_stdout 0 count_chars_in_line ''
+check_stdout 5 count_chars_in_line '     '
+check_stdout 5 count_chars_in_line '$hoge'
+check_stdout 5 count_chars_in_line '*hoge'
+check_stdout 8 count_chars_in_line 'hoge.txt'
+check_stdout 10 count_chars_in_line '// /* */ #'
+check_stdout 80 count_chars_in_line '12345678901234567890123456789012345678901234567890123456789012345678901234567890'
+check_stdout 80 count_chars_in_line 'ああああああああああああああああああああああああああああああああああああああああ'
+check_stdout 96 count_chars_in_line "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん！？"
--- test.sh
+++ test.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-check() {
-  expected=$1
-  script_name=$2
-  args=( "${@:3}" )
+check_stdout() {
+  local expected=$1
+  local script_name=$2
+  local args=( "${@:3}" )
   echo -n "$script_name ${args[*]}..."
   actual="$($script_name "${args[@]}")"
   if [ "$expected" != "$actual" ]; then
     echo
-    echo "'$expected' expected, but got '$actual.'"
+    echo "  '$expected' expected, but got '$actual'."
     exit 1
   else
     echo OK
   fi
 }
-
-check 'This command requires exactly one argument.' timer
-check 'invalid argument.' timer not_a_number
-check 'invalid argument.' timer 12ab
-check 'invalid argument.' timer ab12
